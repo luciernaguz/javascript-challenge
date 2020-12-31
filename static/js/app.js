fc03836cc820778234f776be6f042c981bb6ac15
luciernaguz <luciernaguz@hotmail.com>
2020-12-30 19:57:16 -0600

    Update structure

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Assign the data from `data.js` to a descriptive variable
+//var people = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  //var inputElement = d3.select("#btn btn-default");
+ 
+  //var inputDate = d3.select("#filter-btn");
+  // Get the value property of the input element
+  //var inputValue = inputElement.property("value");
+
+  console.log(button);
+  console.log(tableData);
+
+//  var filteredData = people.filter(person => person.bloodType === inputValue);
+
+//  console.log(filteredData);
+
+
+
+});
